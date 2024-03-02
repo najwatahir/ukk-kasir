5870147bfd2ce72ee8e3b4697bcb595e5a53f910
Najwa Tahir <najwatahir186@gmail.com>
2024-03-02 13:58:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -93,7 +93,7 @@ int main() {
         scanf("%d", &nomor_barang);
 
         if (nomor_barang == 99) {
-            total = 0; // reset subtotal
+            total = 0; // reset btotal
             total_diskon = 0;
             total_setelah_diskon = 0;
             // mengecek barang mana yang dapat diskon
